7b9466d1e74d288311332ad679af42134a20e11a
Dilipsinh Jadeja <70471636+dvjadeja@users.noreply.github.com>
2021-08-11 15:13:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from "axios";
+import { useState } from "react";
+import styled from "styled-components";
+import Movie from "./components/Movie";
+import MovieDetail from "./components/MovieDetail";
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div `
+  background-color: black;
+  color: white;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+`;
+
+const AppName = styled.div `
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+`;
+
+const InputBox = styled.div `
+  display: flex;
+  flex-direction: row;
+  padding: 10px 10px;
+  border-radius: 6px;
+  margin-left: 20px;
+  width: 50%;
+  background-color: white;
+`;
+
+const SearchInput = styled.input`
+  color: black;
+  font-size: 16px;
+  font-weight: bold;
+  border: none;
+  outline: none;
+  margin-left: 15px;
+`;
+
+const MovieListContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  padding: 30px;
+  gap: 25px;
+  justify-content: space-evenly; ;
+`;
+
+export const API_KEY = '89981625a6a2af635d7b04894195ebe2'
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState();
+  const [timeoutId, setTimeoutId] = useState();
+  const [movieList, setMovieList] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState();
+
+  const fetchData = async (searchString) => {
+    const response = await axios.get(`http://api.themoviedb.org/3/search/movie?query=${searchQuery}&api_key=${API_KEY}`)
+    console.log(response);
+    setMovieList(response.data.results)
+  }
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId)
+    setSearchQuery(event.target.value);
+    const timeout = setTimeout(() => fetchData(event.target.value), 1000);
+    setTimeoutId(timeout);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>
+        <AppName>
+          Movie Search App
+        </AppName>
+        <InputBox>
+          <SearchInput placeholder="Enter Movie Name" onChange={onTextChange}/>
+        </InputBox>
+      </Header>
+      {selectedMovie && <MovieDetail selectedMovie={selectedMovie} API_KEY={API_KEY} onMovieSelect={setSelectedMovie}/>}
+      <MovieListContainer>
+        {
+          movieList?.length ? movieList.map((movie, index) => <Movie key={index} movie={movie} onMovieSelect={setSelectedMovie} />) : 'No Movie Found'
+        }
+      </MovieListContainer>
+    </Container>
   );
 }
 
---
+++ src/components/Movie.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import defaultImg from './3793096.jpg'
+
+const MovieContainer = styled.div `
+  display: flex;
+  flex-direction: column;
+  padding: 10px;
+  width: 280px;
+  box-shadow: 0 3px 10px 0 #aaa;
+  cursor: pointer;
+`;
+
+const CoverImg = styled.img `
+    height: 362px;
+    object-fit: cover;
+`;
+
+const MovieName = styled.span `
+    font-size: 18px;
+    font-weight: 600;
+    color: black;
+    margin: 15px 0;
+    white-space: nowrap;
+    text-overflow: ellipsis;
+    overflow: hidden;
+`;
+
+const MovieInfo = styled.span`
+  font-size: 16px;
+  font-weight: 500;
+  color: black;
+  white-space: nowrap;
+  overflow: hidden;
+  text-transform: capitalize;
+  text-overflow: ellipsis;
+`;
+
+
+const Movie = (props) => {
+    const {id, title,poster_path, popularity} = props.movie;
+    let Image = `http://image.tmdb.org/t/p/w185${poster_path}`
+    return (
+        <MovieContainer onClick={() => props.onMovieSelect(id)}>
+            <CoverImg  src={poster_path === null ? defaultImg : Image} />
+            <MovieName>{title}</MovieName>
+            <MovieInfo>{popularity}%</MovieInfo>
+        </MovieContainer>
+    );
+}
+
+export default Movie
